0752cd2e26fd62d0f327b12a44ef48a7f11df2b3
mzlnk <dev.zielonka.marcin@gmail.com>
2020-01-27 18:13:21 +0100

    refactor

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "[api-key-here]",
+    authDomain: "course-master-agh.firebaseapp.com",
+    databaseURL: "https://course-master-agh.firebaseio.com",
+    projectId: "course-master-agh",
+    storageBucket: "course-master-agh.appspot.com",
+    messagingSenderId: "972909190431",
+    appId: "1:972909190431:web:7165bb379f7484f9c1a42a"
+  }
+};
