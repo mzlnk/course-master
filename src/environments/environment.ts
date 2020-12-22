9a9d8f3cf9fd92dc63bcfe3b40192704551ee955
mzlnk <44784576+mzlnk@users.noreply.github.com>
2020-12-23 00:07:55 +0100

    refactor - added env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
-
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC3Qsva208Q8gA1yahkmQw0ZFT1oWEvu7s",
-    authDomain: "course-master-agh.firebaseapp.com",
-    databaseURL: "https://course-master-agh.firebaseio.com",
-    projectId: "course-master-agh",
-    storageBucket: "course-master-agh.appspot.com",
-    messagingSenderId: "972909190431",
-    appId: "1:972909190431:web:7165bb379f7484f9c1a42a"
+    apiKey: "[API-KEY-HERE]",
+    authDomain: "[AUTH-DOMAIN]",
+    databaseURL: "[DATABASE-URL]",
+    projectId: "[PROJECT-ID]",
+    storageBucket: "[STORAGE-BUCKET]",
+    messagingSenderId: "[MSG-SENDER-ID]",
+    appId: "[APP-ID]"
   }
 };
